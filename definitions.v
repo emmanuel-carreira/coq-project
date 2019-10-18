db5a4ad7e3060c375b9fd6106b3424c3812358d9
Emmanuel Carreira <30865344+emmanuel-carreira@users.noreply.github.com>
2019-10-18 20:46:37 -0300

    Created definitions.v

---
+++ definitions.v
@@ -0,0 +1,6 @@
+(** 
+Projeto em Coq da disciplina de Tópicos Avançados em Engenharia de Software do CIn/UFPE 
+Objetivo: provar corretude do Algoritmo RSA
+Autores: Emmanuel Carreira Alves
+         Lucas Geraldo Cilento
+*)
