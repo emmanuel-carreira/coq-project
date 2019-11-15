d74507fc27d97353a45bd92b44907b5f1dcd3559
Emmanuel Carreira <eca2@cin.ufpe.br>
2019-11-15 11:12:48 -0300

    Finished lemmas nao_primo

--- proofs.v
+++ proofs.v
@@ -12,19 +12,32 @@ Check primo 13 12.
 Check primo 4 3.
 
 Lemma divide_simetria: forall x : nat, divide x x.
+Proof. Admitted.
 
 Lemma divide_antisimetria: forall x y : nat, divides x y -> divides y x -> x = y.
+Proof. Admitted.
 
 Lemma nao_primo_0 : ~ primo 0.
+Proof.
+  simpl. reflexivity.
+Qed.
 
 Lemma nao_primo_1 : ~ primo 1.
+Proof.
+  simpl. reflexivity.
+Qed.
 
 Theorem divide_soma: forall x y z : nat, divide x y -> divide x z -> divide x (y + z).
+Proof. Admitted.
 
 Theorem euclides: forall a b c m : nat, divide c (m * a) -> divide c (m * b) -> divide c (m * gcd a b).
+Proof. Admitted.
 
 Lemma potencia_mult: forall x n m : nat, power x n * power x m = power x (n + m).
+Proof. Admitted.
 
 Lemma potencia_de_potencia: forall x n m : nat, power (power x n) m = power x (n * m).
+Proof. Admitted.
 
 Theorem divide_congruencia : forall x : nat, divide n x -> congruent 0 x.
+Proof. Admitted.
