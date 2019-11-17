039666fdb88155a8fdb33fd4ad92c8e3b75f8a33
Emmanuel Carreira <eca2@cin.ufpe.br>
2019-11-17 10:47:32 -0300

    Improved legibility. Fixed some compilation errors in proofs

--- proofs.v
+++ proofs.v
@@ -22,7 +22,6 @@ Require Import Zdiv.
 Require Import Wf_nat.
 Require Import Omega.
 
-
 (*Lemma divide_simetria: forall x : nat, divide x x.
 
 Lemma divide_antisimetria: forall x y : nat, divides x y -> divides y x -> x = y.
@@ -31,19 +30,23 @@ Lemma nao_primo_0 : ~ primo 0.
 
 Lemma nao_primo_1 : ~ primo 1.*)
 
+
 Axiom totientePrimo : forall x : nat, verificaPrimo x = true -> totiente x = x -1.
+
 
 Theorem divide_soma: forall x y z : nat, divide x y -> divide x z -> divide x (y + z) .
 Proof.
   intros x y z.
   simpl. intros H. intros H2. simpl. induction y as [O |  n].
   - simpl. apply H2. 
-  -  Admitted.
-  
+  - Admitted.
+
+
 Theorem euclides: forall a b c m : nat, divide c (m * a) -> divide c (m * b)-> divide c (m * gcd a b).
 Proof.
 intros.
-  Admitted.
+Admitted.
+
 
 Theorem pow_1_l: forall n : nat, 1 ^ n = 1.
 Proof.
@@ -51,11 +54,14 @@ Proof.
   induction n.
   - auto.
   - simpl. rewrite -> IHn. auto.
- Qed.
+Qed.
+
+
 Theorem pow_n_0: forall n : nat, n ^ 0 = 1.
 Proof.
   intros. intuition.
- Qed.
+Qed.
+
 
 Theorem aXaton: forall n a: nat, a *(a ^ n) = a  ^ (n + 1).
 Proof.
@@ -63,28 +69,40 @@ Proof.
   induction n.
   - auto.
   - simpl. rewrite -> IHn. auto.
- Qed.
+Qed.
+
+
 Theorem nX0: forall n : nat, 0*n = 0.
 Proof.
   intros. auto.
 Qed.
+
+
 Theorem oXn: forall n : nat, n*0 = 0.
 Proof.
   intros. auto.
 Qed.
+
+
 Theorem nX1: forall n : nat, n *1 = n.
 Proof.
   intros. intuition.
 Qed.
+
+
 Theorem add_commutative: forall n m : nat, n + m = m + n.
 Proof.
   intros. intuition.
 Qed.
+
+
 Lemma mult_plus_distr_l : forall n m p, n * (m + p) = n * m + n * p.
 Proof.
   induction n. trivial.
   intros. simpl in |- *. rewrite (IHn m p). apply sym_eq. apply plus_permute_2_in_4.
 Qed.
+
+
 Theorem n_plus_nXm: forall n m : nat, n + n*m = n*(m + 1).
 Proof.
   intros. induction n.
@@ -93,18 +111,23 @@ Proof.
     + intuition. 
     + rewrite -> mult_plus_distr_l. intuition. 
 Qed. 
+
+
 Lemma mult_assoc_reverse : forall n m p, n * m * p = n * (m * p).
 Proof.
   intros; elim n; intros; simpl in |- *; auto with arith.
   rewrite mult_plus_distr_r.
   elim H; auto with arith.
 Qed.
+
+
 Lemma mult_comm : forall n m, n * m = m * n.
 Proof.
 intros; elim n; intros; simpl in |- *; auto with arith.
 elim mult_n_Sm.
 elim H; apply plus_comm.
 Qed.
+
 
 Theorem n_plus_mXn: forall n m : nat, n + m*n = n*(m + 1).
 Proof.
@@ -116,12 +139,14 @@ Proof.
      rewrite <- mult_comm. auto.
 Qed. 
 
+
 Lemma potencia_mult: forall x n m : nat,  (x ^ n) * (x ^ m) =  x  ^ (n + m).
 Proof.
   intros. auto. simpl. induction n as [O | n'].
   - auto. simpl. auto.
   - simpl. rewrite <- IHn'. intuition.
 Qed. 
+
 
 Lemma pow_0_n: forall n :nat, n > 0 -> 0 ^ n = 0.
 Proof.
@@ -138,17 +163,22 @@ Proof.
   - simpl. rewrite -> aXaton. rewrite -> n_plus_mXn. rewrite -> mult_comm.
     induction m.
     + rewrite -> mult_comm. rewrite -> pow_n_0. rewrite -> mult_plus_distr_l. simpl. reflexivity.
-    +simpl. rewrite <- mult_comm. intuition. rewrite <- mult_plus_distr_l.
-Theorem  totienteNumeroPrimo: forall x : nat, verificaPrimo x = true-> totiente x = x - 1.
+    +simpl. rewrite <- mult_comm. intuition. (*rewrite <- mult_plus_distr_l.*)
+Admitted.
+
+
+Theorem totienteNumeroPrimo: forall x : nat, verificaPrimo x = true-> totiente x = x - 1.
 Proof.
 intros. unfold totiente. destruct verificaCoPrimos.
   - auto.
   - auto.
   - Admitted.
-Theorem  a_divide_b: forall a b : nat, b mod a = 0 -> divide a b = true.
+
+
+Theorem  a_divide_b: forall a b : nat, b mod a = 0 -> divide a b = True.
 Proof.
 intros a b.  unfold divide. destruct divide.
   - auto.
-  - induction b.  
-    + simpl. Admitted.
-    
+  - induction b.
+    + simpl.
+Admitted.
