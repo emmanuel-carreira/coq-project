18c1427ac16c2c1103cd4d554786ba25de4b056a
lucas cilento <lgc3@cin.ufpe.br>
2019-11-18 00:42:19 -0300

    modified:   proofs.v Avanços na prova de cifraDecifra

--- proofs.v
+++ proofs.v
@@ -19,15 +19,12 @@ Require Import Wf_nat.
 Require Import Omega.
 Require Import  NProperties OrdersFacts.
 
-<<<<<<< HEAD
 Module Nat
  <: NAxiomsSig
  <: UsualDecidableTypeFull
  <: OrderedTypeFull
  <: TotalOrder.
 
-=======
->>>>>>> 039666fdb88155a8fdb33fd4ad92c8e3b75f8a33
 (*Lemma divide_simetria: forall x : nat, divide x x.
 
 Lemma divide_antisimetria: forall x y : nat, divides x y -> divides y x -> x = y.
@@ -35,7 +32,6 @@ Lemma divide_antisimetria: forall x y : nat, divides x y -> divides y x -> x = y
 Lemma nao_primo_0 : ~ primo 0.
 
 Lemma nao_primo_1 : ~ primo 1.*)
-
 
 Axiom totientePrimo : forall x : nat, verificaPrimo x = true -> totiente x = x -1.
 
@@ -45,6 +41,8 @@ Axiom mod0eqdiv: forall x y: nat, y mod x = 0-> divide x y.
 
 Axiom dividePred: forall x y: nat, (y -1 ) mod x =0 -> 1 = y mod x.
 
+Axiom dividePred2: forall x y: nat, 1 = y mod x -> (y -1 ) mod x =0.
+
 Axiom swapMod: forall x y: nat, y > 1 ->1 =  y mod x  -> y = 1 mod x.
 
 Axiom pot_pot: forall  m e n d : nat , (m ^ e mod n) ^ d mod n = (m ^(e * d)) mod n.
@@ -53,11 +51,16 @@ Axiom o_mod_n: forall n : nat,  0 mod n = 0.
 
 Axiom mod_dist: forall a b c : nat, a*b mod c = (a mod c) * (b mod c).
 
+Axiom mod__: forall a b  : nat, a < b -> a mod b = a.
+
+Axiom pow_n_1: forall n: nat, n ^ 1 = n.
+
+Axiom Euler_exp_totient: forall a n :nat , verificaCoPrimos a n = true -> a ^ totiente n  = 1 mod n.
+
 Theorem euclides: forall a b c m : nat, divide c (m * a) -> divide c (m * b)-> divide c (m * gcd a b).
 Proof.
 intros.
-Admitted.
-
+  Admitted.
 
 Theorem pow_1_l: forall n : nat, 1 ^ n = 1.
 Proof.
@@ -65,14 +68,11 @@ Proof.
   induction n.
   - auto.
   - simpl. rewrite -> IHn. auto.
-Qed.
-
-
+ Qed.
 Theorem pow_n_0: forall n : nat, n ^ 0 = 1.
 Proof.
   intros. intuition.
-Qed.
-
+ Qed.
 
 Theorem aXaton: forall n a: nat, a *(a ^ n) = a  ^ (n + 1).
 Proof.
@@ -80,40 +80,28 @@ Proof.
   induction n.
   - auto.
   - simpl. rewrite -> IHn. auto.
-Qed.
-
-
+ Qed.
 Theorem nX0: forall n : nat, 0*n = 0.
 Proof.
   intros. auto.
 Qed.
-
-
 Theorem oXn: forall n : nat, n*0 = 0.
 Proof.
   intros. auto.
 Qed.
-
-
 Theorem nX1: forall n : nat, n *1 = n.
 Proof.
   intros. intuition.
 Qed.
-
-
 Theorem add_commutative: forall n m : nat, n + m = m + n.
 Proof.
   intros. intuition.
 Qed.
-
-
 Lemma mult_plus_distr_l : forall n m p, n * (m + p) = n * m + n * p.
 Proof.
   induction n. trivial.
   intros. simpl in |- *. rewrite (IHn m p). apply sym_eq. apply plus_permute_2_in_4.
 Qed.
-
-
 Theorem n_plus_nXm: forall n m : nat, n + n*m = n*(m + 1).
 Proof.
   intros. induction n.
@@ -122,23 +110,18 @@ Proof.
     + intuition. 
     + rewrite -> mult_plus_distr_l. intuition. 
 Qed. 
-
-
 Lemma mult_assoc_reverse : forall n m p, n * m * p = n * (m * p).
 Proof.
   intros; elim n; intros; simpl in |- *; auto with arith.
   rewrite mult_plus_distr_r.
   elim H; auto with arith.
 Qed.
-
-
 Lemma mult_comm : forall n m, n * m = m * n.
 Proof.
 intros; elim n; intros; simpl in |- *; auto with arith.
 elim mult_n_Sm.
 elim H; apply plus_comm.
 Qed.
-
 
 Theorem n_plus_mXn: forall n m : nat, n + m*n = n*(m + 1).
 Proof.
@@ -158,7 +141,6 @@ Qed.
 
 
 
-
 Lemma potencia_mult: forall x n m : nat,  (x ^ n) * (x ^ m) =  x  ^ (n + m).
 Proof.
   intros. auto. simpl. induction n as [O | n'].
@@ -166,12 +148,7 @@ Proof.
   - simpl. rewrite <- IHn'. intuition.
 Qed.
 
-<<<<<<< HEAD
 Theorem divide_soma: forall x y z : nat, divide x y -> divide x z -> divide x (y + z) .
-=======
-
-Lemma pow_0_n: forall n :nat, n > 0 -> 0 ^ n = 0.
->>>>>>> 039666fdb88155a8fdb33fd4ad92c8e3b75f8a33
 Proof.
   intros x y z.
   simpl. intros H. intros H2. simpl. induction y as [O |  n].
@@ -239,15 +216,16 @@ intros.
     + simpl. rewrite -> o_mod_n. rewrite -> oXn. rewrite -> o_mod_n. rewrite -> oXn. reflexivity.
     + unfold mult.  intuition. Admitted. 
 
-Theorem cifraDecifra: forall m c e d p q n a b, e > 1 -> d > 1 -> a = e*d -> b = totiente n-> n = p*q -> 1 = e*d mod (totiente n) -> a >1-> c = encriptaNumero m n e -> decriptaNumero c n d = m.
+Definition divide x y := exists z, y=z*x.
+Notation "( x | y )" := (divide x y) (at level 0) : nat_scope.
+
+Axiom exists_multiple: forall a b: nat, a mod b = 0 <> exists c, a = b*c.
+
+Axiom exists_multiple2: forall a b: nat, divide b a -> 0 = a mod b.
+
+
+Theorem cifraDecifra: forall m c e d p q n a b, e > 1 -> d > 1 -> a = e*d -> b = totiente n-> n = p*q -> 1 = e*d mod (totiente n) -> a >1-> c = encriptaNumero m n e -> m < n -> decriptaNumero c n d = m.
 Proof.
 intros. unfold decriptaNumero. rewrite -> H6. unfold encriptaNumero. rewrite -> pot_pot. rewrite -> aux2. rewrite <- H1. rewrite <- potencia_mult.  
-  - rewrite -> mod_dist. rewrite <- H2 in H4. rewrite <- H1 in H4. Admitted.
-
-Theorem  a_divide_b: forall a b : nat, b mod a = 0 -> divide a b = True.
-Proof.
-intros a b.  unfold divide. destruct divide.
-  - auto.
-  - induction b.
-    + simpl.
-Admitted.
+  - rewrite -> mod_dist.  rewrite <- H1 in H4. rewrite -> pow_n_1.  rewrite -> mult_comm. rewrite -> mod__. apply dividePred2 in H4. rewrite <- H2 in H4. rewrite <- exists_multiple2 in H4. 
+     
