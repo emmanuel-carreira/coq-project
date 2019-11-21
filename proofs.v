ee84b473c302f2e796faea2cb9ab0453f263dae1
Emmanuel Carreira <eca2@cin.ufpe.br>
2019-11-20 22:04:34 -0300

    Changed axioms to lemmas

--- proofs.v
+++ proofs.v
@@ -25,46 +25,66 @@ Module Nat
  <: OrderedTypeFull
  <: TotalOrder.
 
-(*Lemma divide_simetria: forall x : nat, divide x x.
+Lemma totientePrimo : forall x : nat, verificaPrimo x = true -> totiente x = x -1.
+Proof.
+Admitted.
 
-Lemma divide_antisimetria: forall x y : nat, divides x y -> divides y x -> x = y.
+Lemma totienteMult : forall x y: nat, totiente (x*y) = (totiente x) * (totiente y).
+Proof.
+Admitted.
 
-Lemma nao_primo_0 : ~ primo 0.
+Lemma mod0eqdiv: forall x y: nat, y mod x = 0-> divide x y.
+Proof.
+Admitted.
 
-Lemma nao_primo_1 : ~ primo 1.*)
+Lemma dividePred: forall x y: nat, (y -1 ) mod x =0 -> 1 = y mod x.
+Proof.
+Admitted.
 
-Axiom totientePrimo : forall x : nat, verificaPrimo x = true -> totiente x = x -1.
+Lemma dividePred2: forall x y: nat, 1 = y mod x -> (y -1 ) mod x =0.
+Proof.
+Admitted.
 
-Axiom totienteMult : forall x y: nat, totiente (x*y) = (totiente x) * (totiente y).
+Lemma swapMod: forall x y: nat, y > 1 ->1 =  y mod x  -> y = 1 mod x.
+Proof.
+Admitted.
 
-Axiom mod0eqdiv: forall x y: nat, y mod x = 0-> divide x y.
+Lemma pot_pot: forall  m e n d : nat , (m ^ e mod n) ^ d mod n = (m ^(e * d)) mod n.
+Proof.
+Admitted.
 
-Axiom dividePred: forall x y: nat, (y -1 ) mod x =0 -> 1 = y mod x.
+Lemma pot_pot2: forall m e d: nat, (m ^ e) ^ d = m ^ (e*d).
+Proof.
+Admitted.
 
-Axiom dividePred2: forall x y: nat, 1 = y mod x -> (y -1 ) mod x =0.
+Lemma o_mod_n: forall n : nat,  0 mod n = 0.
+Proof.
+Admitted.
 
-Axiom swapMod: forall x y: nat, y > 1 ->1 =  y mod x  -> y = 1 mod x.
+Lemma mod_dist: forall a b c : nat, a*b mod c = (a mod c) * (b mod c).
+Proof.
+Admitted.
 
-Axiom pot_pot: forall  m e n d : nat , (m ^ e mod n) ^ d mod n = (m ^(e * d)) mod n.
+Lemma mod__: forall a b  : nat, a < b -> a mod b = a.
+Proof.
+Admitted.
 
-Axiom pot_pot2: forall m e d: nat, (m ^ e) ^ d = m ^ (e*d).
+Lemma one_mod_m: forall m : nat, m > 1-> 1 mod m = 1.
+Proof.
+Admitted.
 
-Axiom o_mod_n: forall n : nat,  0 mod n = 0.
+Lemma pow_n_1: forall n: nat, n ^ 1 = n.
+Proof.
+Admitted.
 
-Axiom mod_dist: forall a b c : nat, a*b mod c = (a mod c) * (b mod c).
-
-Axiom mod__: forall a b  : nat, a < b -> a mod b = a.
-
-Axiom one_mod_m: forall m : nat, m > 1-> 1 mod m = 1.
-
-Axiom pow_n_1: forall n: nat, n ^ 1 = n.
-
-Axiom Euler_exp_totient: forall a n :nat , verificaCoPrimos a n = true -> a ^ totiente n  = 1 mod n.
+Lemma Euler_exp_totient: forall a n :nat , verificaCoPrimos a n = true -> a ^ totiente n  = 1 mod n.
+Proof.
+Admitted.
 
 Theorem euclides: forall a b c m : nat, divide c (m * a) -> divide c (m * b)-> divide c (m * gcd a b).
 Proof.
 intros.
-  Admitted.
+Admitted.
 
 Theorem pow_1_l: forall n : nat, 1 ^ n = 1.
 Proof.
@@ -72,7 +92,8 @@ Proof.
   induction n.
   - auto.
   - simpl. rewrite -> IHn. auto.
- Qed.
+Qed.
+
 Theorem pow_n_0: forall n : nat, n ^ 0 = 1.
 Proof.
   intros. intuition.
@@ -84,28 +105,34 @@ Proof.
   induction n.
   - auto.
   - simpl. rewrite -> IHn. auto.
- Qed.
+Qed.
+
 Theorem nX0: forall n : nat, 0*n = 0.
 Proof.
   intros. auto.
 Qed.
+
 Theorem oXn: forall n : nat, n*0 = 0.
 Proof.
   intros. auto.
 Qed.
+
 Theorem nX1: forall n : nat, n *1 = n.
 Proof.
   intros. intuition.
 Qed.
+
 Theorem add_commutative: forall n m : nat, n + m = m + n.
 Proof.
   intros. intuition.
 Qed.
+
 Lemma mult_plus_distr_l : forall n m p, n * (m + p) = n * m + n * p.
 Proof.
   induction n. trivial.
   intros. simpl in |- *. rewrite (IHn m p). apply sym_eq. apply plus_permute_2_in_4.
 Qed.
+
 Theorem n_plus_nXm: forall n m : nat, n + n*m = n*(m + 1).
 Proof.
   intros. induction n.
@@ -113,7 +140,8 @@ Proof.
   - induction m.
     + intuition. 
     + rewrite -> mult_plus_distr_l. intuition. 
-Qed. 
+Qed.
+
 Lemma mult_assoc_reverse : forall n m p, n * m * p = n * (m * p).
 Proof.
   intros; elim n; intros; simpl in |- *; auto with arith.
@@ -135,15 +163,14 @@ Proof.
     + intuition. 
     + rewrite -> mult_plus_distr_l. rewrite -> nX1. rewrite -> add_commutative.
      rewrite <- mult_comm. auto.
-Qed. 
+Qed.
+
 Lemma pow_0_n: forall n :nat, n > 0 -> 0 ^ n = 0.
 Proof.
 intros n. induction n.
   - intuition.
   - intuition.
 Qed.
-
-
 
 Lemma potencia_mult: forall x n m : nat,  (x ^ n) * (x ^ m) =  x  ^ (n + m).
 Proof.
@@ -159,7 +186,9 @@ Proof.
   - simpl. apply H2. 
   - simpl. induction z.
     + simpl. rewrite -> add_commutative. simpl. apply H.
-    + simpl.  Admitted.
+    + simpl.
+Admitted.
+
 Lemma mod_1_l: forall a, 1<a -> 1 mod a = 1.
 Proof.
 intros. simpl. Admitted.
@@ -172,8 +201,8 @@ Proof.
   - simpl. rewrite -> aXaton. rewrite -> n_plus_mXn. rewrite -> mult_comm.
     induction m.
     + rewrite -> mult_comm. rewrite -> pow_n_0. rewrite -> mult_plus_distr_l. simpl. reflexivity.
-    +simpl. rewrite <- mult_comm. intuition. Admitted.
-
+    +simpl. rewrite <- mult_comm. intuition.
+Admitted.
 
 Theorem  totienteN: forall p q : nat, verificaPrimo p = true /\ verificaPrimo q = true-> totiente (p*q) =  (p - 1)*(q - 1).
 Proof.
@@ -182,7 +211,6 @@ intros. rewrite -> totienteMult. inversion H. apply totientePrimo in H0.
 Qed.
 
 Theorem  aux: forall p : nat, p > 1-> p = (p -1) +1.
-
 Proof.
 intros. induction p.
   - simpl. intuition.
@@ -210,7 +238,6 @@ intros.  rewrite <- aux.  reflexivity. induction p.
 Qed.
 
 
-  
 Theorem mod_dist2: forall a b c : nat, a*b mod c = (a mod c) * (b mod c).
 Proof.
 intros.
@@ -223,13 +250,21 @@ intros.
 Definition divide x y := exists z, y=z*x.
 Notation "( x | y )" := (divide x y) (at level 0) : nat_scope.
 
-Axiom exists_multiple: forall a b: nat, a mod b = 0 <> exists c, a = b*c.
+Lemma exists_multiple: forall a b: nat, a mod b = 0 <> exists c, a = b*c.
+Proof.
+Admitted.
 
-Axiom exists_multiple2: forall a b: nat, a mod b = 0 -> divide b a.
+Lemma exists_multiple2: forall a b: nat, a mod b = 0 -> divide b a.
+Proof.
+Admitted.
 
-Axiom Zpower_mod p q n : forall n p q : nat, 0 < n -> (p^q) mod n = ((p mod n)^q) mod n.
+Lemma Zpower_mod p q n : forall n p q : nat, 0 < n -> (p^q) mod n = ((p mod n)^q) mod n.
+Proof.
+Admitted.
 
-Axiom pot_pot_3: forall n p q : nat, 0 < n -> (p^q) ^ n = (p^n) ^q.
+Lemma pot_pot_3: forall n p q : nat, 0 < n -> (p^q) ^ n = (p^n) ^q.
+Proof.
+Admitted.
 
 Lemma mult_gt_one: forall m n: nat, m > 1 -> n > 1 -> m*n > 1.
 Proof. intros. unfold mult. 
@@ -243,6 +278,6 @@ Proof.
 intros. unfold decriptaNumero. rewrite -> H8. unfold encriptaNumero. rewrite -> pot_pot. rewrite -> aux2. rewrite <- H3. rewrite <- potencia_mult.  
   - rewrite -> mod_dist.  rewrite <- H3 in H6. rewrite -> pow_n_1.  rewrite -> mult_comm. rewrite -> mod__. apply dividePred2 in H6. rewrite <- H4 in H6.
   apply exists_multiple2 in H6. unfold divide in H6.
-  destruct H6 as [z]. rewrite H6. rewrite -> mult_comm. rewrite <- pot_pot2.  rewrite -> pot_pot_3. rewrite -> Zpower_mod. rewrite -> H4. rewrite -> Euler_exp_totient. rewrite -> one_mod_m. rewrite one_mod_m. rewrite -> pow_1_l. rewrite -> one_mod_m. intuition. repeat (rewrite -> H5. apply mult_gt_one. apply H.). apply H0.  
+  destruct H6 as [z]. rewrite H6. rewrite -> mult_comm. rewrite <- pot_pot2.  rewrite -> pot_pot_3. rewrite -> Zpower_mod. rewrite -> H4. rewrite -> Euler_exp_totient. rewrite -> one_mod_m. rewrite one_mod_m. rewrite -> pow_1_l. rewrite -> one_mod_m. intuition. repeat rewrite -> H5. apply mult_gt_one. apply H. apply H0.
   -
      
